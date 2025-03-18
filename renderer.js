310bf065b9cba8058bdd6f7fbd5f82c4bd64b6ca
David <77506410+gdavidev@users.noreply.github.com>
2025-03-18 17:39:18 -0300

    refact/feat: renderer.js refactored and score counter implemented

--- renderer.js
+++ renderer.js
@@ -1,75 +1,100 @@
+import { shuffle } from './libs/ArrayUtilities.js'
+import { replaceChildren } from './libs/HtmlElementsUtilities.js'
+
+class PageContent {
+  questionTextElement = document.getElementById('question');
+  dialogMessageElement = document.getElementById('result-message');
+  dialog = document.getElementById('dialog-result');
+  nextQuestionButton = document.getElementById('next-button');
+  alternativesContainer = document.getElementById('alternatives-container');
+  scoreElement = document.getElementById('score');
+  maxScoreElement = document.getElementById('max-score');
+  
+  score = 0;
+  maxScore = 0;
+  
+  loadAlternatives(alternatives, answer) {
+    const buttons = alternatives.map(a => createAlternativeButton(a, a === answer))
+    replaceChildren(this.alternativesContainer, buttons);
+  }
+  
+  setScore(score) {
+    this.score = score;
+    this.scoreElement.textContent = score;
+  }
+  
+  setMaxScore(maxScore) {
+    this.maxScore = maxScore;
+    this.maxScoreElement.textContent = maxScore;
+  }
+  
+  setQuestionText(text) {
+    this.questionTextElement.textContent = text;
+  }
+  
+  setDialogText(text) {
+    this.dialogMessageElement.textContent = text;
+  }
+  
+  showDialog() {
+    this.dialog.style.display = 'block';
+  }
+  
+  hideDialog() {
+    this.dialog.style.display = 'none';
+  }
+  
+  setOnNextButtonClick(callback) {
+    this.nextQuestionButton.onclick = callback;
+  }
+}
+let pageContent;
+
 window.bridge.onJsonData((event, data) => {
-  initializePage(data);
+  pageContent = new PageContent();
+  
+  const shuffledQuestions = shuffleQuestions(data)
+  initializePage(shuffledQuestions);
 });
 
+function shuffleQuestions(data) {
+  data.forEach(d => { d.alternatives = shuffle(d.alternatives) });
+  return shuffle(data)
+}
+
 function initializePage(questionsData) {
-  const shuffledQuestions = shuffle(questionsData);
+  pageContent.setMaxScore(questionsData.length);
+  pageContent.setScore(0);
   
-  let currentQuestion = 0
-  const nextQuestion = () => {
-    currentQuestion++;
-    console.log(currentQuestion);
-    loadQuestion(shuffledQuestions[currentQuestion], nextQuestion)
+  const nextQuestion = (questionIndex) => {
+    loadQuestion(
+        questionsData[questionIndex],
+        () => nextQuestion(questionIndex + 1)
+    );
   }
-  // Load First
-  loadQuestion(shuffledQuestions[0], nextQuestion);
+  nextQuestion(0);
 }
 
 function loadQuestion(question, nextQuestionCallbackFn) {
-  const buttonsContainer = document.getElementById('alternatives-container');
-  const buttons = question.alternatives.map(a => createAlternativeButton(a, a === question.answer));
-  
-  const questionTextElement = document.getElementById('question');
-  questionTextElement.textContent = question.question;
-  
-  const nextButton = document.getElementById('next-button');
-  nextButton.onclick = nextQuestionCallbackFn;
-  
-  hideDialog();
-  
-  clearButtonsContainer(buttonsContainer);
-  for (let i = 0; i < buttons.length; i++) {
-    buttonsContainer.appendChild(buttons[i]);
-  }
+  pageContent.setOnNextButtonClick(nextQuestionCallbackFn);
+  pageContent.loadAlternatives(question.alternatives, question.answer);
+  pageContent.setQuestionText(question.title);
+  pageContent.hideDialog();
 }
 
 function createAlternativeButton(text, isCorrect) {
   const button = document.createElement('button');
-  button.onclick =  () => showDialog(isCorrect);
+  button.onclick =  () => {
+    if (isCorrect) {
+      pageContent.setDialogText("Você acertou!");
+      pageContent.setScore(pageContent.score + 1);
+    } else {
+      pageContent.setDialogText("Você errou!");
+    }
+    pageContent.showDialog();
+  }
   button.classList.add('alternative-btn');
   button.textContent = text;
   
   return button;
-}
-
-function showDialog(isCorrect) {
-  const dialog = document.getElementById('dialog-result');
-  const dialogMessage = document.getElementById('result-message');
-  dialogMessage.textContent = isCorrect ? 'You did it' : 'You\'re dumb';
-  dialog.style.display = 'block';
-}
-
-function hideDialog() {
-  const dialog = document.getElementById('dialog-result');
-  dialog.style.display = 'none';
-}
-
-function clearButtonsContainer(container) {
-  while (container.lastElementChild) {
-    container.removeChild(container.lastElementChild);
-  }
-}
-
-function shuffle(array) {
-  let subjectArray = array.slice();
-  let currentIndex = array.length;
-  
-  while (currentIndex !== 0) {
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    
-    [subjectArray[currentIndex], subjectArray[randomIndex]] = [subjectArray[randomIndex], subjectArray[currentIndex]];
-  }
-  
-  return subjectArray;
 }
