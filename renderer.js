fe3c9f25e3e0d8ff3cb2b47b85b36d94af7d32a5
David <77506410+gdavidev@users.noreply.github.com>
2025-03-20 23:39:01 -0300

    feat: core gameplay completed and styled

--- renderer.js
+++ renderer.js
@@ -1,24 +1,151 @@
-import { shuffle } from './libs/ArrayUtilities.js'
-import { replaceChildren } from './libs/HtmlElementsUtilities.js'
+import { shuffle } from './libs/ArrayUtilities.js';
 
 class PageContent {
-  questionTextElement = document.getElementById('question');
-  dialogMessageElement = document.getElementById('result-message');
-  dialog = document.getElementById('dialog-result');
-  nextQuestionButton = document.getElementById('next-button');
+  // Elements
+  questionTextElement = document.getElementById('question-text');
   alternativesButtonsArr = document.getElementsByClassName('alternative-btn');
   scoreElement = document.getElementById('score');
-  maxScoreElement = document.getElementById('max-score');
+  timeBarElement = document.getElementById('time-bar');
+  resultTextElement = document.getElementById('result-text');
+  resultDisplayContainerElement = document.getElementById('result-display-container');
+  resultScoreTextElement = document.getElementById('result-score-text');
+  currentQuestionElement = document.getElementById('progress');
+  questionAmountElement = document.getElementById('max-progress');
   
+  // Data
+  questions = [];
+  
+  // Configuration
+  scoreMultiplier = 10;
+  timePerQuestionMs = 5000;
+  timeOnResultsViewMs = 1500;
+  numOfQuestions = 5;
+  
+  // State
   score = 0;
-  maxScore = 0;
+  currentQuestion = 0;
+  timeWhenStarted = 0;
+  timeBarAnimation = null;
+  timeOutRef = null;
   
-  loadAlternatives(alternatives, answer) {
+  loadPage(data) {
+    const shuffledQuestions = shuffleQuestions(data);
+    this.questions = shuffledQuestions.slice(0, this.numOfQuestions);
+    this.questionAmountElement.textContent = String(this.questions.length);
+    this.currentQuestionElement.textContent = '1';
+    this.timeBarAnimation = this.createTimeBarAnimation()
+    this.setScore(0);
+    
+    this.switchToQuestionView();
+    
+    this.loadQuestion(this.questions[0]);
+  }
+  
+  nextQuestion() {
+    this.currentQuestion++;
+    this.currentQuestionElement.textContent = String(this.currentQuestion + 1);
+    this.loadQuestion(this.questions[this.currentQuestion]);
+  }
+  
+  loadQuestion(question) {
+    this.loadAlternatives(question.alternatives);
+    this.setQuestionText(question.title);
+  }
+  
+  loadAlternatives(alternatives) {
     for (let i = 0; i < this.alternativesButtonsArr.length; i++) {
-      const isCorrect = alternatives[i] === answer
-
       this.alternativesButtonsArr[i].textContent = alternatives[i];
-      this.alternativesButtonsArr[i].onclick = () => this.showDialogResult(isCorrect);
+      this.alternativesButtonsArr[i].onclick = () => {
+        this.choseAlternative(alternatives[i]);
+      }
+    }
+  }
+  
+  choseAlternative(alternative) {
+    const isCorrect = this.questions[this.currentQuestion].answer === alternative
+    
+    if (isCorrect) {
+      const scoreGained = (Date.now() - this.timeWhenStarted) * this.scoreMultiplier;
+      
+      this.setScore(this.score + scoreGained);
+      this.switchToResultView('correct', scoreGained)
+    } else {
+      this.switchToResultView('wrong', 0)
+    }
+  }
+  
+  switchToResultView(reason, scoreGained) {
+    this.setAlternativesLocked(true);
+    this.pauseTimer();
+    
+    this.resultDisplayContainerElement.style.display = 'block'
+    this.questionTextElement.style.display = 'none';
+    this.resultScoreTextElement.textContent = String(scoreGained)
+    
+    switch (reason) {
+      case 'correct':
+        this.resultTextElement.textContent = "Parabéns"
+        this.resultTextElement.style.color = 'green'
+        break;
+      case 'wrong':
+        this.resultTextElement.textContent = "Você Errou!"
+        this.resultTextElement.style.color = 'red'
+        break;
+      case 'time-out':
+        this.resultTextElement.textContent = "O tempo acabou!"
+        this.resultTextElement.style.color = 'red'
+        break;
+    }
+    
+    setTimeout(() => {
+      this.switchToQuestionView();
+      this.nextQuestion();
+    }, this.timeOnResultsViewMs)
+  }
+  
+  switchToQuestionView() {
+    this.setAlternativesLocked(false);
+    this.timeBarElement.style.animationPlayState = 'running'
+    
+    this.resultDisplayContainerElement.style.display = 'none';
+    this.questionTextElement.style.display = 'block';
+    
+    this.startTimer()
+  }
+  
+  startTimer() {
+    this.timeWhenStarted = Date.now();
+    this.timeBarAnimation.finish()
+    this.timeBarAnimation.play()
+    
+    clearTimeout(this.timeOutRef)
+    this.timeOutRef = setTimeout(() => {
+      this.switchToResultView('time-out', 0)
+    }, this.timePerQuestionMs);
+  }
+  
+  pauseTimer() {
+    clearTimeout(this.timeOutRef);
+    this.timeBarAnimation.pause();
+  }
+  
+  createTimeBarAnimation() {
+    return this.timeBarElement.animate([
+      { 'background': 'green', 'width': '100%', },
+      { 'background': 'red', 'width': '0%', }
+    ], {
+      duration: this.timePerQuestionMs,
+    })
+  }
+  
+  setAlternativesLocked(isLocked) {
+    for (let i = 0; i < this.alternativesButtonsArr.length; i++) {
+      if (isLocked) {
+        this.alternativesButtonsArr[i].classList.add('btn-locked');
+        this.alternativesButtonsArr[i].onclick = undefined; // Remove event
+      } else {
+        this.alternativesButtonsArr[i].classList.remove('btn-locked');
+      }
     }
   }
   
@@ -27,68 +154,22 @@ class PageContent {
     this.scoreElement.textContent = score;
   }
   
-  setMaxScore(maxScore) {
-    this.maxScore = maxScore;
-    this.maxScoreElement.textContent = maxScore;
-  }
-  
   setQuestionText(text) {
     this.questionTextElement.textContent = text;
   }
-  
-  setDialogText(text) {
-    this.dialogMessageElement.textContent = text;
-  }
-  
-  showDialogResult(isCorrect) {
-    if (isCorrect) {
-      this.setDialogText("Você acertou!");
-      this.setScore(this.score + 1);
-    } else {
-      this.setDialogText("Você errou!");
-    }
-    this.dialog.style.display = 'block';
-  }
-  
-  hideDialog() {
-    this.dialog.style.display = 'none';
-  }
-  
-  setOnNextButtonClick(callback) {
-    this.nextQuestionButton.onclick = callback;
-  }
 }
 let pageContent;
+let questionsDatabase;
 
+// Initialize page trigger
 window.bridge.onJsonData((event, data) => {
-  pageContent = new PageContent();
+  questionsDatabase = data;
   
-  const shuffledQuestions = shuffleQuestions(data);
-  const chosenQuestions = shuffledQuestions.slice(0, 5);
-  initializePage(chosenQuestions);
+  pageContent = new PageContent();
+  pageContent.loadPage(data);
 });
 
 function shuffleQuestions(data) {
   data.forEach(d => { d.alternatives = shuffle(d.alternatives) });
   return shuffle(data);
-}
-
-function initializePage(questionsData) {
-  pageContent.setMaxScore(questionsData.length);
-  pageContent.setScore(0);
-  
-  const nextQuestion = (questionIndex) => {
-    loadQuestion(
-        questionsData[questionIndex],
-        () => nextQuestion(questionIndex + 1)
-    );
-  }
-  nextQuestion(0);
-}
-
-function loadQuestion(question, nextQuestionCallbackFn) {
-  pageContent.setOnNextButtonClick(nextQuestionCallbackFn);
-  pageContent.loadAlternatives(question.alternatives, question.answer);
-  pageContent.setQuestionText(question.title);
-  pageContent.hideDialog();
 }
